57adf443f35817b3aeade7e7b83ea300e4917162
Carlos Sosa <carlos.alberto.sosa.perera@gmail.com>
2026-06-15 01:54:10 -0600

    feat: using spa over ssr

--- src/router.tsx
+++ src/router.tsx
@@ -1,7 +1,5 @@
 import { createRouter as createTanStackRouter } from '@tanstack/react-router'
 import { routeTree } from './routeTree.gen'
-
-import { setupRouterSsrQueryIntegration } from '@tanstack/react-router-ssr-query'
 import { getContext } from './integrations/tanstack-query/root-provider'
 
 export function getRouter() {
@@ -10,12 +8,7 @@ export function getRouter() {
   const router = createTanStackRouter({
     routeTree,
     context,
-    scrollRestoration: true,
-    defaultPreload: 'intent',
-    defaultPreloadStaleTime: 0,
   })
-
-  setupRouterSsrQueryIntegration({ router, queryClient: context.queryClient })
 
   return router
 }
